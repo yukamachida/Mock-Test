917d904b36fa2aeffe5c9c9b6c7dd325ae3b0016
yuka machida <sh0ge0405@gmail.com>
2025-03-18 16:56:42 +0900

    registerの再ルーティング

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -6,40 +6,46 @@ use Illuminate\Http\Request;
 use App\Models\User;
 class AuthController extends Controller
 {
-    public function create(Request $request)
+
+    public function register()
     {
-        $form = $request->all();
-        User::create($form); //createメソッドでテーブルにデータを挿入
-        return view('/');
+        return view('auth.register');
+
     }
+    // public function create(Request $request)
+    // {
+    //     $form = $request->all();
+    //     User::create($form); //createメソッドでテーブルにデータを挿入
+    //     return view('/');
+    // }
 
 
     public function index()
     {
-        
-        return view('index');
+        dd;
+        return view('layouts.index');
     }
 
 
-    public function login(request $request)
-    {
-        $form = $request->all();
-        User::create($form);
-        return redirect('/mypage/profile');
+    // public function login(request $request)
+    // {
+    //     $form = $request->all();
+    //     User::create($form);
+    //     return redirect('/mypage/profile');
 
-    }
+    // }
 
-    public function show_profile()
+    public function show_profile_setting()
     {
         return view('auth.first_login');
     }
+
+
     public function profile_setting(request $request)
     {
-        
         $form = $request->all();
         User::create($form);
 
-        return redirect('index');
-
+        return redirect('mypage/profile');
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,22 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-//Route::get('/mypage/profile', [AuthController::class, 'show_profile']);
+Route::get('register/', [AuthController::class, 'register']);
+Route::post('register/', [AuthController::class, 'profile_setting']);
+Route::get('/mypage/profile', [AuthController::class, 'show_profile_setting']);
+Route::get('/', [AuthController::class, 'index']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/', [AuthController::class, 'index']);
+   
     Route::post('/', [AuthController::class, 'index']);
     //Route::post('/register', [AuthController::class, 'profile_setting']);
     //Route::get('/mypage/profile', [AuthController::class, 'show_profile']);
