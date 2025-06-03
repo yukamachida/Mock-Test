6af9aeb4ca9d27fdaba82fd92365083fda94bb9a
yuka machida <sh0ge0405@gmail.com>
2025-06-04 00:18:13 +0900

    show変更

--- src/resources/views/profile/show.blade.php
+++ src/resources/views/profile/show.blade.php
@@ -4,6 +4,25 @@
     <a href="/mypage/profile">プロフィールを編集</a>
 
     <h2>プロフィール画面</h2>
+    <div Class="toppage-list">
+        <div class="toppage-list_list">
+            <a href="/mypage?tab=sell">出品した商品</a>
+            <a href="/mypage?tab=buy">購入した商品</a>
+        </div>
+    </div>
+    <div class="product-sample-data">
+        @foreach ($products as $item)
+            @php
+                $product = $viewType === 'sell' ? $item : $item->product;
+            @endphp
+            <div class="product-sample-data">
+                <img src="{{ asset($product->image) }}" alt="商品画像" width="150">
+                <p>{{ $product->name }}</p>
+            </div>
+        @endforeach
+    </div>
+
+
+
 
 @endsection
-   
